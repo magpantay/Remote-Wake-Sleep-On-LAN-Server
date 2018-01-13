5d4e7bfb0825ba3f9720fa4667470cc2e5005a35
Kyran Magpantay <magpantay.kyran@gmail.com>
2018-01-12 23:25:51 -0800

    Quick change
    - The bracket addition placement has been changed slightly

--- index.php
+++ index.php
@@ -164,12 +164,12 @@ else
 					if ($pinginfo == "")
 					{
 						$asleep = true;
-						echo "<h5>" . $COMPUTER_NAME[$selectedComputer] . " is currently [ <span style='color:#CC0000';>asleep</span> ].</h5>";
+						echo "<h5>" . $COMPUTER_NAME[$selectedComputer] . " is currently <span style='color:#CC0000';>[asleep]</span>.</h5>";
 					}
 					else
 					{
 						$asleep = false;
-						echo "<h5>" . $COMPUTER_NAME[$selectedComputer] . " is currently [ <span style='color:#00CC00;'>awake</span> ].</h5>";
+						echo "<h5>" . $COMPUTER_NAME[$selectedComputer] . " is currently [ <span style='color:#00CC00;'>[awake]</span>.</h5>";
 					}
 				}
 
